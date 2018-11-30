dbdf210ae83982d0c41b59d1514cc1a4af7b629f
Mini01 <skekdwoals@naver.com>
2018-11-30 09:38:56 +0900

    first

---
+++ src/com/biz/raffle/NamesClass.java
@@ -0,0 +1,16 @@
+package com.biz.raffle;
+
+public class NamesClass {
+	
+	// final NAMES 변수는 읽기 전용으로된다
+	public static final String[] NAMES =  {
+			"홍길동",
+			"이몽룡",
+			"성춘향",
+			"임꺽정",
+			"허난설혀",
+			"허균"
+			
+	};
+
+}
